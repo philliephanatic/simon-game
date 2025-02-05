a5f51c303662d1a4bffa9d3541c656bb5eb22c5c
philliephanatic <nolan.r.young@gmail.com>
2025-02-04 20:29:19 -0500

    add JS: Switch statement for sound, click event listener, stored array

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
-//simon-game 
+let userClickedPattern = [];
 
-//step 1:
-//step 2:
+$(".btn").click( function () {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    
+    console.log(userClickedPattern);
+});
 
+let gamePattern = [];
 
-$(body).css("background-colofr","red");
+const buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence () {
+    let randomNumber = Math.floor(Math.random() * buttonColors.length);
+    
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    
+    $("#" + randomChosenColor).addClass("pressed");
+    
+    setTimeout(() => {
+        $("#" + randomChosenColor).removeClass("pressed");
+    }, 200);
+
+    playSound(randomChosenColor);
+
+}
+
+function playSound(color) {
+    switch (color) {
+        case "blue":
+            const blue = new Audio("sounds/blue.mp3").play()
+            break;
+    
+        case "green":
+            const green = new Audio("sounds/green.mp3").play()
+            break;
+    
+        case "red" :
+            const red = new Audio("sounds/red.mp3").play()
+            break;
+
+        case "yellow":
+            const yellow = new Audio("sounds/yellow.mp3").play()
+            break;
+
+        case "wrong":
+            const wrong = new Audio("sounds/wrong.mp3").play()
+            break;
+            
+}};
+
+nextSequence();
